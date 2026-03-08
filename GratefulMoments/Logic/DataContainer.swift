6b3a3f93998498be3589487a9854facd2eb92839
app-kaisha <app.kaihatsusha@gmail.com>
2026-03-08 18:58:00 +0000

    Data Container with sample data created

---
+++ GratefulMoments/Logic/DataContainer.swift
@@ -0,0 +1,53 @@
+//
+//  DataContainer.swift
+//  GratefulMoments
+//
+//  Created by app-kaihatsusha on 08/03/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftData
+import SwiftUI
+
+@Observable
+@MainActor
+class DataContainer {
+    let modelContainer: ModelContainer
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    init(includeSampleMoments: Bool = false) {
+        let schema = Schema([Moment.self])
+        
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: includeSampleMoments)
+        
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            if includeSampleMoments {
+                loadSampleMoments()
+            }
+            try context.save()
+            
+        } catch {
+            fatalError("Could not crate ModelContainer: \(error)")
+        }
+    }
+    
+    private func loadSampleMoments() {
+        for moment in Moment.sampleData {
+            context.insert(moment)
+        }
+    }
+}
+
+private let sampleContainer = DataContainer(includeSampleMoments: true)
+
+extension View {
+    func sampleDataContainer() -> some View {
+        self
+            .environment(sampleContainer)
+            .modelContainer(sampleContainer.modelContainer)
+    }
+}
