b25f0d699bb18eaadb343f5d3e1a8b6db0ee1314
app-kaisha <app.kaihatsusha@gmail.com>
2026-03-08 20:45:20 +0000

    HexagonView integrated into moments scrollview

--- GratefulMoments/Tabs/Moments/MomentsView.swift
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -16,6 +16,8 @@ struct MomentsView: View {
     @Query(sort: \Moment.timestamp)
     private var moments: [Moment]
     
+    static let offsetAmount: CGFloat = 70.0
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -44,19 +46,30 @@ struct MomentsView: View {
 
                 }
             }
+            .defaultScrollAnchor(.bottom, for: .initialOffset)
+            .defaultScrollAnchor(.bottom, for: .sizeChanges)
+            .defaultScrollAnchor(.top, for: .alignment)
             .navigationTitle("Grateful Moments")
         }
     }
     
     private var pathItems: some View {
-        ForEach(moments) { moment in
+        ForEach(moments.enumerated(), id:\.0) { index, moment in
             NavigationLink {
                 MomentDetailView(moment: moment)
             } label: {
-                Text(moment.title)
+                if moment == moments.last {
+                    MomentHexagonView(moment: moment, layout: .large)
+                } else {
+                    MomentHexagonView(moment: moment)
+                        .offset(x: sin(Double(index) * .pi / 2) * Self.offsetAmount)
+                }
             }
-
-            
+            .scrollTransition { content, phase in
+                content
+                    .opacity(phase.isIdentity ? 1 : 0)
+                    .scaleEffect(phase.isIdentity ? 1 : 0.8)
+            }
         }
     }
 }
@@ -69,4 +82,5 @@ struct MomentsView: View {
 #Preview("No Moments") {
     MomentsView()
         .modelContainer(for: [Moment.self])
+        .environment(DataContainer())
 }
