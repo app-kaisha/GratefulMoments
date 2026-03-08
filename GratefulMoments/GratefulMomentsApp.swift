90e72faa573ce04ea7f65ec318171c9bce202da9
app-kaisha <app.kaihatsusha@gmail.com>
2026-03-08 17:51:04 +0000

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,18 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by app-kaihatsusha on 08/03/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
